8668da548c0acfce0fd85c038bda65c9f136b148
Tochilina Polina <narkada@mail.ru>
2022-02-16 20:55:08 +0300

    Start clean architecture

---
+++ domain/src/main/java/com/example/boardgamefinder/domain/repository/UserRepository.kt
@@ -0,0 +1,13 @@
+package com.example.boardgamefinder.domain.repository
+
+import com.example.boardgamefinder.domain.models.UserCredentials
+
+/**
+ * Interface for interacting with user data
+ */
+interface UserRepository {
+    /**
+     * User registration
+     */
+    fun register(credentials: UserCredentials): Result<Boolean>
+}
