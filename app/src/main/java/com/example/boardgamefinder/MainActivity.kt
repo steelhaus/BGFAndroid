8118bdc539ea77214121c3303be596f040029dcb
Tochilina Polina <narkada@mail.ru>
2022-02-16 18:51:23 +0300

    Google map key storage, base navigation

--- app/src/main/java/com/example/boardgamefinder/MainActivity.kt
+++ app/src/main/java/com/example/boardgamefinder/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.example.boardgamefinder
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.boardgamefinder.databinding.ActivityMainBinding
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // setting fragments for main frame
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.nav_home -> {
+                    replaceFragment(HomeFragment())
+                }
+                R.id.nav_search -> {
+                    replaceFragment(SearchFragment())
+                }
+                R.id.nav_add -> {
+                    replaceFragment(AddFragment())
+                }
+                R.id.nav_notifications -> {
+                    replaceFragment(NotificationsFragment())
+                }
+                R.id.nav_profile -> {
+                    replaceFragment(ProfileFragment())
+                }
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        // open fragment
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_frame, fragment)
+            .commit()
     }
 }
